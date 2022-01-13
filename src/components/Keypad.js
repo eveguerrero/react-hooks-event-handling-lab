bc375202439c2df9883ae8b808a6806bedabcce2
Eveline Guerrero <evelineguerrero23@gmail.com>
2022-01-13 08:01:43 -0500

    commit tree 0f068431405550913b7b4812e9ce5876cc95f3e0

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function Keypad(){
+    function pass(){
+        return console.log('Entering password...')
+    }
+return (
+<input type="password"  onChange={pass}/>
+)}
+
+export default Keypad;
